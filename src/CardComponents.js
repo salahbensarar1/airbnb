1ca19abec011900e41f6d77fb9c5e80d5a748a18
salah ben sarar <sa.bensarar@gmail.com>
2024-02-21 14:04:46 +0100

    Some CSS/React Modifications

--- src/CardComponents.js
+++ src/CardComponents.js
@@ -4,20 +4,24 @@ import image1 from "./assets/image1.png"
 import wedding_photography from "./assets/wedding_photography.png"
 import mountain_bike1 from "./assets/mountain_bike1.png"
 import Surfing from "./assets/Surfing.png"
+import MountianView from "./assets/MountianView.png"
 
 const cardData = [
     { id: 1, image: image1, rating: 5.0, reviews: "(6) •", location: 'USA', title: 'Life Lessons with Katie Zaferes', price: 136 } ,
     { id: 2, image: wedding_photography, rating: 5.0, reviews: "(30) •", location: 'USA', title:'Learn Wedding Photography', price: 125} ,
     { id: 3, image: mountain_bike1, rating: 4.8, reviews: '(2) •', location: 'USA', titile: 'Group Mountain Biking', price: 50 } ,
-    { id: 4, image: Surfing, rating: 4.8, reviews: '(2) •', location: 'USA', titile: 'Group Mountain Biking', price: 50 } ,
-    { id: 5, image: mountain_bike1, rating: 4.8, reviews: '(2) •', location: 'USA', titile: 'Group Mountain Biking', price: 50 } 
+    { id: 4, image: Surfing, rating: 4.8, reviews: '(2) •', location: 'USA', titile: 'Group Surfing ', price: 50 } ,
+    { id: 5, image: MountianView, rating: 4.8, reviews: '(2) •', location: 'USA', titile: ' Mountain Hiking', price: 50 } ,
+    { id: 6, image: MountianView, rating: 4.8, reviews: '(2) •', location: 'USA', titile: ' Mountain Hiking', price: 50 },
 ];
 export default function CardContainer() {
     return (
+        <div className="CardContainerWrapper">
         <div className="CardContainer">
             {cardData.map((card) => (
                 <Card key={card.id} {...card} />
             ))}
+        </div>
         </div>
     );
 }
